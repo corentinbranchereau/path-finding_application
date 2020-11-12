71a81c5d3c34c6ad49d2011f86cf0ca086aa2f45
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-12 15:54:05 +0100

    Create Maven Project + GitIgnore + JUnit/Mockito

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 4IF_PLD_AGILE
 Agile Long Duration Project at INSA Lyon - Pick &amp; Delivery
+
+#MAVEN
+
+##Installation
+http://maven.apache.org/download.cgi
+
+##Help
+https://www.journaldev.com/33645/maven-commands-options-cheat-sheet
---
+++ src/test/java/fr/hexaone/AppTest.java
@@ -0,0 +1,21 @@
+package fr.hexaone;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
