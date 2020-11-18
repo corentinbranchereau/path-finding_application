106067a4cda2133757d4170f09dabf945a76da63
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-18 11:45:03 +0100

    Squelette de javadoc pour utils

--- src/main/java/fr/hexaone/utils/XMLDeserializer.java
+++ src/main/java/fr/hexaone/utils/XMLDeserializer.java
@@ -3,20 +3,33 @@ package fr.hexaone.utils;
 import fr.hexaone.model.Planning;
 import fr.hexaone.model.Carte;
 
+/**
+ * Permet de déserialiser un fichier au format XML.
+ * Classe statique.
+ * @author HexaOne
+ * @version 1.0
+ */
+
 import java.io.File;
 
 public class XMLDeserializer {
 
-    public XMLDeserializer(){
-
+    /**
+     * Charge les données d'un fichier XML bien formé contenant des intersections et des segments formant la carte
+     * @param carte La carte où charger les données.
+     * @param file Le fichier XML bien formé contenant les données.
+     */
+    public static void loadMap(Carte carte, File file){
+        //TODO
     }
 
-    public void loadMap(Carte carte, File file){
-
-    }
-
-    public void loadRequete(Planning planning, File file){
-
+    /**
+     * Charge les données d'un fichier XML bien formé contenant les requêtes formant le planning
+     * @param planning Le planning où charger les données.
+     * @param file Le fichier XML bien formé contenant les données.
+     */
+    public static void loadRequete(Planning planning, File file){
+        //TODO
     }
 
 }
--- src/main/java/fr/hexaone/utils/XMLFileOpener.java
+++ src/main/java/fr/hexaone/utils/XMLFileOpener.java
@@ -2,21 +2,55 @@ package fr.hexaone.utils;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileNotFoundException;
 
+/**
+ * Permet d'ouvrir un fichier et de version type (XML).
+ * Implémente FileFilter du package java.io
+ * @see FileFilter
+ * @author HexaOne
+ * @version 1.0
+ */
 public class XMLFileOpener implements FileFilter {
 
-    public XMLFileOpener(){
+    /**
+     * L'instance SINGLETON de la classe.
+     */
+    private static XMLFileOpener instance = null;
 
+    /**
+     * Constructeur par défaut privé.
+     */
+    private XMLFileOpener(){}
+
+    /**
+     * Implémentation du design pattern SINGLETON afin d'implémenter l'interface FileFilter du package java.io
+     * en gardant un comportement static pour la classe
+     * @return l'unique instance de la classe XMLFileOpener. Si elle n'existe pas, l'instancie.
+     */
+    protected static XMLFileOpener getInstance(){
+        if(instance == null) instance = new XMLFileOpener();
+        return instance;
     }
 
-    public File open(String path){
+    /**
+     * Ouverture d'un fichier dont l'URI est spécifié en paramètre
+     * @param path L'URI du fichier
+     * @throws FileNotFoundException Lorsque l'URI spécifié n'existe pas
+     * @return Le fichier ouvert
+     */
+    public File open(String path) throws FileNotFoundException {
+        //TODO
         return null;
     }
 
+    /**
+     * Vérifie l'extension du fichier ouvert
+     * @param file Le fichier à vérifier
+     * @return true si le fichier est au format XML, false sinon.
+     */
     @Override
-    public boolean accept(File pathname) {
-        return false;
+    public boolean accept(File file) {
+        return file.getName().endsWith(".xml");
     }
-
-
 }
