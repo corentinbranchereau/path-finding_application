ce62ada703c5007b2be9f710d4435b683c622e04
corentinbranchereau <branchereaucorentin@gmail.com>
2020-11-18 15:43:06 +0100

    doc(view): created view package

---
+++ src/main/java/fr/hexaone/view/ButtonListener.java
@@ -0,0 +1,16 @@
+package fr.hexaone.view;
+
+import javax.swing.Action;
+
+/**
+ * Gère les évènements de click sur les boutons de l'IHM.
+ * @author HexaOne
+ * @version 1.0
+ */
+
+public class ButtonListener{
+    
+    public ButtonListener(){
+
+    }
+}
