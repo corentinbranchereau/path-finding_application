3a0259c8d01f53379dcb5780513402ee1ee3c314
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-18 11:17:17 +0100

    init packages

---
+++ src/main/java/fr/hexaone/utils/XMLDeserializer.java
@@ -0,0 +1,22 @@
+package fr.hexaone.utils;
+
+import fr.hexaone.model.Planning;
+import fr.hexaone.model.Carte;
+
+import java.io.File;
+
+public class XMLDeserializer {
+
+    public XMLDeserializer(){
+
+    }
+
+    public void loadMap(Carte carte, File file){
+
+    }
+
+    public void loadRequete(Planning planning, File file){
+
+    }
+
+}
---
+++ src/main/java/fr/hexaone/utils/XMLFileOpener.java
@@ -0,0 +1,22 @@
+package fr.hexaone.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class XMLFileOpener implements FileFilter {
+
+    public XMLFileOpener(){
+
+    }
+
+    public File open(String path){
+        return null;
+    }
+
+    @Override
+    public boolean accept(File pathname) {
+        return false;
+    }
+
+
+}
