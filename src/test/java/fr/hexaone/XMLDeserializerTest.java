4dd47571cc032224b465cc0daac541e278cf025c
lperrious <louis30@me.com>
2020-11-19 14:42:53 +0100

    test : adding XMLDeserializerTest.java

---
+++ src/test/java/fr/hexaone/XMLDeserializerTest.java
@@ -0,0 +1,19 @@
+package fr.hexaone;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class XMLDeserializerTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
