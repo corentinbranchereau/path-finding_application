6baf101601aaa0554463267eff1ab195d463feda
koechslin <killian.oechslin@insa-lyon.fr>
2020-11-19 19:20:30 +0100

    feat: move launch and start methods in App.java

--- src/main/java/fr/hexaone/App.java
+++ src/main/java/fr/hexaone/App.java
@@ -1,13 +1,30 @@
 package fr.hexaone;
 
+import fr.hexaone.controller.Controleur;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
- * Hello world!
- *
+ * Permet de lancer l'application et de créer le controleur.
+ * 
+ * @author HexaOne
+ * @version 1.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App extends Application {
+
+    /**
+     * Cette méthode démarre l'application et instancie le controleur.
+     * 
+     * @param stage Variable propre à JavaFX permettant d'afficher les éléments à
+     *              l'écran
+     */
+    @Override
+    public void start(Stage stage) {
+        Controleur controleur = new Controleur(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(App.class, args);
     }
 }
