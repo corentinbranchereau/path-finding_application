8bd143ebd46f67164e6b4b870d087d15f33352bb
lperrious <louis30@me.com>
2020-11-19 14:04:30 +0100

    fix: erreur commentaire coco

--- src/main/java/fr/hexaone/view/VueTextuelle.java
+++ src/main/java/fr/hexaone/view/VueTextuelle.java
@@ -7,32 +7,35 @@ import fr.hexaone.model.Planning;
 
 /**
  * Permet d'afficher la partie textuelle de l'IHM.
+ * 
  * @author HexaOne
  * @version 1.0
  */
 
 public class VueTextuelle {
-    
+
     /**
      * constructeur
      */
-    public VueTextuelle(){
+    public VueTextuelle() {
 
     }
 
     /**
      * Méthode qui permet d'afficher le planning dans la vue textuelle
+     * 
      * @param planning liste des segments à parcourir
      */
     public void afficherPlanning(Planning planning) {
-        
+
     }
-    
+
     /**
      * Méthode qui permet d'afficher les requêtes dans la vue textuelle
+     * 
      * @param listeRequetes
      */
-    //public void afficherRequetes(List<Requete> listeRequetes) {
-        
-    }
+    // public void afficherRequetes(List<Requete> listeRequetes) {
+
+    // }
 }
