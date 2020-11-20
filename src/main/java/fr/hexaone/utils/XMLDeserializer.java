2fa686157453ad209fd0a3fc1ee510d150a708c0
lperrious <louis30@me.com>
2020-11-20 17:21:36 +0100

    feat: start of loadRequete in XMLDeserializer

--- src/main/java/fr/hexaone/utils/XMLDeserializer.java
+++ src/main/java/fr/hexaone/utils/XMLDeserializer.java
@@ -1,7 +1,14 @@
 package fr.hexaone.utils;
 
 import fr.hexaone.model.Planning;
+import fr.hexaone.model.Requete;
 import fr.hexaone.model.Carte;
+import fr.hexaone.model.Depot;
+import fr.hexaone.model.IntersectionSpeciale;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Permet de déserialiser un fichier au format XML.
@@ -11,6 +18,9 @@ import fr.hexaone.model.Carte;
  */
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XMLDeserializer {
 
@@ -30,10 +40,31 @@ public class XMLDeserializer {
      * le planning
      * 
      * @param planning Le planning où charger les données.
-     * @param file     Le fichier XML bien formé contenant les données.
+     * @param xml      Le fichier XML bien formé contenant les données.
      */
     public static void loadRequete(Planning planning, Document xml) {
-        // TODO
-    }
+        try {
+            // On récupère le depot
+            Element depotTag = (Element) xml.getElementsByTagName("depot");
+            // !planning.setDepot(new Depot(depotTag.getAttribute("address"),
+            // depotTag.getAttribute("departureTime")));
+            // On construit la liste des requetes
+            List<Requete> listRequetes = new ArrayList<>();
+            NodeList requetesNode = xml.getElementsByTagName("request");
+            for (int i = 0; i < requetesNode.getLength(); ++i) {
+                Element requeteTag = (Element) requetesNode.item(i);
+                // ! Problème : Quand on récupère une requete, on a l'id du pickup et du
+                // delivery. Ils correspondent à des intersections existant déjà par ailleurs en
+                // mémoire. Mais on y a pas accès (à moins de passer la Map en param) et si on y
+                // a accès comment les modifier en intersectionSpeciale ? De plus on ne peut pas
+                // créer les intersectionsSpeciales directement car on ne connait pas lat et
+                // long et ça ferait des doublons d'id ...
 
+                // listRequetes.add(new Requete(...));
+            }
+            planning.setRequetes(listRequetes);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
