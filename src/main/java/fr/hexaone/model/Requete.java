f844342937419a2c99e9a232b77b51feeaa4cdec
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-20 14:58:46 +0100

    doc: Requete

--- src/main/java/fr/hexaone/model/Requete.java
+++ src/main/java/fr/hexaone/model/Requete.java
@@ -2,24 +2,24 @@ package fr.hexaone.model;
 
 /**
  * Objet permettant de modéliser une requête, c'est à dire avec un point de
- * retrait un point de livraison"
+ * retrait (PICKUP) et un point de livraison (DELIVERY)
  *
  * @author HexaOne
  * @version 1.0
  */
 public class Requete {
     /**
-     * intersection spécaiale de type collecte
+     * Id de l'intersection spéciale de type collecte
      */
     protected long idPickup;
 
     /**
-     * intersection spéciale de type point de livraison
+     * Id l'intersection spéciale de type point de livraison
      */
     protected long idDelivery;
 
     /**
-     * constructeur de Requete
+     * Constructeur de Requete
      *
      * @param idPickup
      * @param idDelivery
