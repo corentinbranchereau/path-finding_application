28ef53ed1f87b8fcd656021c2a78fcebbecc309e
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-26 00:23:07 +0100

    feat : structure of ParserErrorHandler

---
+++ src/main/java/fr/hexaone/utils/ParserErrorHandler.java
@@ -0,0 +1,22 @@
+package fr.hexaone.utils;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class ParserErrorHandler implements ErrorHandler {
+    @Override
+    public void warning(SAXParseException exception) throws SAXException {
+        
+    }
+
+    @Override
+    public void error(SAXParseException exception) throws SAXException {
+
+    }
+
+    @Override
+    public void fatalError(SAXParseException exception) throws SAXException {
+
+    }
+}
