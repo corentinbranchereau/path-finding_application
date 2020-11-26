4dccb777ed0b422d6606541ca68d2954438b8be9
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-11-26 11:31:29 +0100

    feat: IllegalAttributException

---
+++ src/main/java/fr/hexaone/utils/exception/IllegalAttributException.java
@@ -0,0 +1,19 @@
+package fr.hexaone.utils.exception;
+
+/**
+ * Exception provoqué lorsque le fichier XML provoque une erreur de PARSER
+ * dû à un attribut de type illégal (ex : texte au lieu d'un double).
+ * @see Exception
+ * @author HexaOne
+ * @version 1.0
+ */
+public class IllegalAttributException extends Exception{
+
+    /**
+     * Héritage d'une exception java
+     */
+    public IllegalAttributException(String errorMessage){
+        super(errorMessage);
+    }
+    
+}
