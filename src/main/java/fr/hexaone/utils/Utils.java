f00fccbc8ba09b47616f42c295e6cc101ab24820
tgravey <38787197+Thibaut468@users.noreply.github.com>
2020-12-06 21:38:25 +0100

    feat: utility method for help

--- src/main/java/fr/hexaone/utils/Utils.java
+++ src/main/java/fr/hexaone/utils/Utils.java
@@ -1,7 +1,12 @@
 package fr.hexaone.utils;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -24,6 +29,42 @@ public class Utils {
         alert.setContentText(message);
         ((Stage)alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("file:src/main/resources/logo-hexa.png"));
         alert.show();
+    }
+
+    /**
+     * Obtenir un titre pour le menu d'aide utilisateur
+     * @param titre Le titre souhaité
+     * @return Le label à afficher
+     */
+    public static Label obtenirTitreAide(String titre){
+        Label label = new Label(titre);
+        label.setFont(Font.font("Segoe UI", FontWeight.BOLD,15D));
+        label.setPadding(new Insets(0D,0D,5D,0D));
+        return label;
+    }
+
+    /**
+     * Obtenir un paragraphe pour le menu d'aide utilisateur
+     * @param texte Le texte souhaité
+     * @return Le label à afficher
+     */
+    public static Label obtenirParagrapheAide(String texte){
+        Label label = new Label(texte);
+        label.setFont(Font.font("Segoe UI", FontWeight.NORMAL, FontPosture.ITALIC,10D));
+        label.setWrapText(true);
+        label.setPadding(new Insets(0D,0D,10D,0D));
+        return label;
+    }
+
+    /**
+     * Obtenir un interligne pour le menu d'aide utilisateur
+     * @param valeur L'intervalle à ajouter
+     * @return Le label à afficher
+     */
+    public static Label obtenirInterligne(double valeur){
+        Label label = new Label("");
+        label.setPadding(new Insets(0D,0D,valeur,0D));
+        return label;
     }
 
 }
